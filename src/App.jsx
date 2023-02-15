bd02809d33621beb1a85261c8a690a2531c424fb
paullaster <paullasterokoth98@gmail.com>
2023-02-15 10:05:23 +0300

    commit files

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import { Form } from 'react-router-dom'
 function App() {
 
   return (
-    <div className="App">
-      <form>
-        <input type="text" name='title'/>
-        <input type="text" name="description"/>
-        <button type='submit'>create</button>
-      </form>
-    </div>
+    <Form>
+      
+    </Form>
   )
 }
 
