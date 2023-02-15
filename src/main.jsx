bb73dfe5611cf610cde34682a838b911f3c98797
paullaster <paullasterokoth98@gmail.com>
2023-02-15 10:49:32 +0300

    commit files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { createBrowserRouter } from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const app = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+    path='/'
+    element={<App/>}
+    />
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <app />
   </React.StrictMode>,
 )
