92018069c2898006989a6b2ce923cb5ebacf2071
paullaster <paullasterokoth98@gmail.com>
2023-02-15 10:08:00 +0300

    commit files

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <Form>
-      
+      <input type="text" name='title' placeholder='Mugging'/>
+      <input type="text" name='description' placeholder='This is the act of ...' />
+      <button type='submit'>create</button>
     </Form>
   )
 }
